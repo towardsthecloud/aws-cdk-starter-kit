be2887ff80ec546fb62fda21000dd9a055828e59
Danny Steenman <danny@towardsthecloud.com>
2025-06-25 09:55:35 +0200

    chore: remove ESLint and Prettier configurations, integrate Biome for code formatting and linting

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,9 @@
 import { awscdk } from 'projen';
 import { DependabotScheduleInterval } from 'projen/lib/github';
 import { NodePackageManager } from 'projen/lib/javascript';
+import { IndentStyle, QuoteStyle, Semicolons, TrailingCommas } from 'projen/lib/javascript/biome/biome-config';
 import { createCdkDeploymentWorkflows } from './src/bin/cicd-helper';
-import { Environment, EnvironmentConfig, addCdkActionTask } from './src/bin/env-helper';
+import { addCdkActionTask, type Environment, type EnvironmentConfig } from './src/bin/env-helper';
 
 // Set the minimum node version for AWS CDK and the GitHub actions workflow
 const nodeVersion = '20.18.1';
@@ -39,19 +40,30 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   projenrcTs: true,
   release: true,
   deps: ['cloudstructs'] /* Runtime dependencies of this module. */,
-  prettier: true,
-  prettierOptions: {
-    settings: {
-      jsxSingleQuote: true,
-      singleQuote: true,
-      printWidth: 120,
-    },
-  },
-  eslintOptions: {
-    prettier: true,
-    dirs: ['src', 'test'],
-    commandOptions: {
-      fix: true,
+  biome: true,
+  biomeOptions: {
+    biomeConfig: {
+      formatter: {
+        enabled: true,
+        useEditorconfig: true,
+        formatWithErrors: false,
+        indentStyle: IndentStyle.SPACE,
+        indentWidth: 2,
+        lineWidth: 120,
+      },
+      javascript: {
+        formatter: {
+          jsxQuoteStyle: QuoteStyle.SINGLE,
+          quoteStyle: QuoteStyle.SINGLE,
+          trailingCommas: TrailingCommas.ALL,
+          semicolons: Semicolons.ALWAYS,
+        },
+      },
+      json: {
+        parser: {
+          allowComments: true,
+        },
+      },
     },
   },
   autoApproveOptions: {
@@ -99,7 +111,6 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   gitignore: [
     '__pycache__',
     '__pycache__/',
-    '!.eslintrc.js',
     '.cache',
     '.coverage.*',
     '.coverage',
